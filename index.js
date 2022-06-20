0d88e62ae90ca1d6b1ccc947b1a4717b3e67ea29
paolaxjean <pr1008631@gmail.com>
2022-06-20 10:39:51 -0700

    deleted some lines of code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// const Employee = require('/lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const path = require('path');
 
 const generateHTML = require('./dist/generateHTML');
 const Team = [];
